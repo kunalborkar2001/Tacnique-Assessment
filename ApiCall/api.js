a29724a6ac925740fbbe1d2c96ee91912b26a453
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-04-19 17:06:58 +0530

    added dynamic data

--- ApiCall/api.js
+++ ApiCall/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { BASE_URL } from '../config.js';
 
-let getAllUsers  = async () => {
+let getAllUsers = async () => {
     let response = await axios.get(`${BASE_URL}/users`)
-    let data = await response.data
-
-    console.log(data);
+   return response
+    
 }
-getAllUsers()
+
+export { getAllUsers }
